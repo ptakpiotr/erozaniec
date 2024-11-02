d6ecf5034235d5b5eef68cf6cd2a788d12d41b99
ptakpiotr <piter020@outlook.com>
2024-11-02 22:33:47 +0100

    Publish info changes

--- Data/PartsHttpService.cs
+++ Data/PartsHttpService.cs
@@ -13,7 +13,7 @@ namespace ERozaniec.Data
 
         public async Task<IList<Models.PartModel>?> GetPartsAsync()
         {
-            return await _client.GetFromJsonAsync<IList<Models.PartModel>>("https://erozaniec.azurewebsites.net/api/InfoFn?code=KCP31CFg6G3wMcDt2hNZs6zMcrC59-5mBx3OV0hA9t4yAzFu1bXybQ%3D%3D")
+            return await _client.GetFromJsonAsync<IList<Models.PartModel>>("https://erozaniec.azurewebsites.net/api/InfoFn")
                 .ConfigureAwait(false);
         }
     }
---
+++ README.md
@@ -0,0 +1,15 @@
+# ERóżaniec
+
+## Table of contents
+- [General info](#general-info)
+- [Examples](#examples)
+
+## General info
+
+Simple app created with .NET MAUI and SQLite.
+
+## Examples
+![img-1.png](Images%2Fimg-1.png)
+![img-2.png](Images%2Fimg-2.png)
+![img-3.png](Images%2Fimg-3.png)
+![img-4.png](Images%2Fimg-4.png)
